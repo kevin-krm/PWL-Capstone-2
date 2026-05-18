de321a108a078e71d5ff59659135fae895e91200
Richard VCD <richardvincentius2112@gmail.com>
2026-05-18 19:00:22 +0700

    Create tampilan dashboard + fix route
    Pembuatan dashboard admin dan kelola pengguna. Memperbaiki routing setelah login

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,24 @@ const { checkAuth } = require('../middlewares/authMiddleware');
 // Hanya 'Administrator' yang bisa akses
 router.use(checkAuth('Administrator'));
 
+// 1. Rute Dashboard
 router.get('/dashboard', (req, res) => {
     res.render('admin/dashboard', { user: req.session.user });
 });
 
+// 2. Rute Kelola Pengguna
 router.get('/users', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) throw err;
         res.render('admin/users', { user: req.session.user, users: results });
+    });
+});
+
+// 3. Rute Kelola Ruangan
+router.get('/rooms', (req, res) => {
+    db.query('SELECT * FROM rooms', (err, results) => {
+        if (err) throw err;
+        res.render('admin/rooms', { user: req.session.user, rooms: results });
     });
 });
 
